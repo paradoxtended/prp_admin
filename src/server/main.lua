215d4fe6da2367d9239b02ab01725a961d50a473
Timothy Barabas <215121906+theravaget@users.noreply.github.com>
2025-06-23 12:56:49 +0200

    banning system needs to be in TS :sadge:

--- src/server/main.lua
+++
@@ -1,8 +0,0 @@
-lib.locale()
-
-lib.addCommand(config.AdminMenu.Command, {
-    help = locale('commandHelp'),
-    restricted = config.AdminMenu.AllowedGroups
-}, function(source)
-    TriggerClientEvent('prp_admin:openAdminMenu', source)
-end)
